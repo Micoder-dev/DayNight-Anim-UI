c01af2ea57dd6c03be8215c9cdee7c2300eab64f
mi-coder <micoder.com@gmail.com>
2023-06-19 16:56:46 +0530

    up

--- app/src/main/java/com/micoder/daynight_anim/MainActivity.java
+++ app/src/main/java/com/micoder/daynight_anim/MainActivity.java
@@ -29,18 +29,15 @@ public class MainActivity extends AppCompatActivity {
 
         sun.setTranslationY(-110);
 
-        dayNightSwitch.setListener(new DayNightSwitchListener() {
-            @Override
-            public void onSwitch(boolean is_night) {
-                if (is_night){
-                    sun.animate().translationY(110).setDuration(1000);
-                    dayLand.animate().alpha(0).setDuration(1300);
-                    daySky.animate().alpha(0).setDuration(1300);
-                }else {
-                    sun.animate().translationY(-110).setDuration(1000);
-                    dayLand.animate().alpha(1).setDuration(1300);
-                    daySky.animate().alpha(1).setDuration(1300);
-                }
+        dayNightSwitch.setListener(is_night -> {
+            if (is_night){
+                sun.animate().translationY(110).setDuration(1000);
+                dayLand.animate().alpha(0).setDuration(1300);
+                daySky.animate().alpha(0).setDuration(1300);
+            }else {
+                sun.animate().translationY(-110).setDuration(1000);
+                dayLand.animate().alpha(1).setDuration(1300);
+                daySky.animate().alpha(1).setDuration(1300);
             }
         });
     }
